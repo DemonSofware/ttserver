19ec7a40af8e14425fc061644921496b00e09c66
demoniskatel <demoniskatel@gmail.com>
2015-05-30 20:18:34 +0300

    server

---
+++ src/tt/TTConnector.java
@@ -0,0 +1,12 @@
+package tt;
+
+public class TTConnector implements ITTConnector {
+	
+	final static String TIME_TABLE_ONLINE = "Time Table Online";
+	
+	@Override
+	public String testFuncion() {
+		return TIME_TABLE_ONLINE;
+	}
+
+}
